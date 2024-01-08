eccce0218fbc69af174acbb2b848bc0a0e3a30cb
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-08 19:07:52 +0300

    Add some test runner code

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import Button from "./src/components/Button";
+import ImageViewer from "./src/components/ImageViewer";
+
+const img = require("./assets/fisrt-page-image.jpg");
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<ImageViewer placeholderImageSource={img}></ImageViewer>
+			<Button label="Press me DADDY" theme="primary"></Button>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		gap: 12,
+		backgroundColor: "#25292e",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	text: {
+		color: "white",
+	},
+	button: {
+		color: "white",
+		backgroundColor: "#4591ff",
+		borderRadius: 12,
+		padding: 9,
+		flexDirection: "row",
+	},
+	buttonIcon: {
+		color: "white",
+		paddingRight: 6,
+	},
 });
